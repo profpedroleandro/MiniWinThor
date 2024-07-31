500a0e63499360e7bdefb3a25aef4aceee8aa4c8
unknown <glaugui454@gmail.com>
2024-07-30 21:43:51 -0300

    implementação dos metodos de excluir e atualizar produtos

--- src/repository/ProdutoRepository.java
+++ src/repository/ProdutoRepository.java
@@ -92,13 +92,59 @@ public class ProdutoRepository {
             System.out.println("****************************** SELECIONAR PRODUTO ******************************");
             System.out.printf("Codigo \t Nome \t Quantidade  \t  Preço \t Status\n");
             while (resultSet.next()) {
-                System.out.printf("%d %s %d %.2f %s\n", resultSet.getInt("idProduto"), resultSet.getString("nome"), resultSet.getInt("quantidade"), resultSet.getDouble("precoVenda"), resultSet.getString("status_produto"));
+            System.out.printf("%d       %s       %d        %.2f       %s\n", resultSet.getInt("idProduto"), resultSet.getString("nome"), resultSet.getInt("quantidade"), resultSet.getDouble("precoVenda"), resultSet.getString("status_produto"));
             }
 
             resultSet.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void atualizarProduto(Produto produto, int id) {
+
+        String comandoSQL = "UPDATE produtos SET nome = ?, descricao = ?, garantia = ?, quantidade = ? ,precoVenda = ?,precoCompra = ? ,dataDaUltimaCompra = ?,status_produto = ? WHERE idProduto = " + id;
+
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = Conexao.getMinhaConexao().prepareStatement(comandoSQL);
+            preparedStatement.setString(1, produto.getNome());
+            preparedStatement.setString(2, produto.getDescricao());
+            preparedStatement.setString(3, produto.getGarantia());
+            preparedStatement.setInt(   4, produto.getQuantidade());
+            preparedStatement.setDouble(5, produto.getPrecoDeVenda());
+            preparedStatement.setDouble(6, produto.getPrecoDeCompra());
+            preparedStatement.setString(7, produto.getDataDaUltimaCompra());
+            preparedStatement.setString(8, produto.getStatusProduto());
+
+
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void excluirProduto(int id) {
+
+        String comandoSQL = "DELETE FROM produtos WHERE idProduto =" + id;
+
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = Conexao.getMinhaConexao().prepareStatement(comandoSQL);
+
+
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
--- src/support/Menu.java
+++ src/support/Menu.java
@@ -14,10 +14,11 @@ public class Menu {
 
         do {
             System.out.println("*************************************MENU*********************************");
-            System.out.println("1.Cadastro de Produtos ");
+            System.out.println("1.Cadastrar de Produto ");
             System.out.println("2.Lista de Produtos ");
-            System.out.println("3.Procurar Produtos ");
-            System.out.println("4.Excluir Produtos ");
+            System.out.println("3.Procurar Produto ");
+            System.out.println("4.Atualizar Produto");
+            System.out.println("5.Excluir Produto ");
             System.out.println("0.Sair");
             System.out.println("**********************************FIM MENU*******************************");
             System.out.println("Selecione uma das opções acima.");
@@ -35,6 +36,12 @@ public class Menu {
                     break;
                 case 3:
                     selecionarProduto();
+                    break;
+                case 4:
+                    atualizarProduto();
+                    break;
+                case 5:
+                    excluirProduto();
                     break;
                 default:
                     System.out.println("Digite uma opção válida");
@@ -77,8 +84,8 @@ public class Menu {
         System.out.println("Informe a data da sua última compra (ano-mês-dia)");
         entrada.nextLine();
         datadaUltimaCompra = entrada.nextLine();
-
         produto.setDataDaUltimaCompra(datadaUltimaCompra);
+
         System.out.println("Informe o status do produto: 1 para 'Ativo' ou 2 para 'Fora de linha'");
         escolha = entrada.nextInt();
 
@@ -113,4 +120,73 @@ public class Menu {
         produtoRepository.selecionarProduto(id);
 
     }
+
+    public static void atualizarProduto(){
+
+        Scanner leia = new Scanner(System.in);
+        Produto produto= new Produto();
+
+        int id;
+
+        String nomedoproduto, descricao, garantia;
+        int quantidade, escolha;
+        double precodeVenda, precodeCompra;
+        String datadaUltimaCompra;
+
+        System.out.println("Insira o id: ");
+        id = leia.nextInt();
+        System.out.println("Digite o nome do produto: ");
+        leia.nextLine();
+        nomedoproduto = leia.nextLine();
+        produto.setNome(nomedoproduto);
+        System.out.println("Digite a descriçao do produto: ");
+        descricao = leia.nextLine();
+        produto.setDescricao(descricao);
+        System.out.println("Digite a garantia do produto: ");
+        garantia = leia.nextLine();
+        produto.setGarantia(garantia);
+        System.out.println("Digite a quantidade do produto: ");
+        quantidade = leia.nextInt();
+        produto.setQuantidade(quantidade);
+        System.out.println("Digite o preço de venda do produto: ");
+        precodeVenda = leia.nextDouble();
+        produto.setPrecoDeVenda(precodeVenda);
+        System.out.println("Digite o preço de compra do produto: ");
+        precodeCompra = leia.nextDouble();
+        produto.setPrecoDeCompra(precodeCompra);
+        System.out.println("Informe a data da sua última compra (ano-mês-dia)");
+        leia.nextLine();
+        datadaUltimaCompra = leia.next();
+        produto.setDataDaUltimaCompra(datadaUltimaCompra);
+
+        System.out.println("Informe o status do produto: 1 para 'Ativo' ou 2 para 'Fora de linha'");
+        escolha = leia.nextInt();
+
+        String status_produto = "ativo";
+
+        if (escolha == 2) {
+            status_produto = "fora de linha";
+        }
+        produto.setStatusProduto(status_produto);
+
+
+        ProdutoRepository produtoRepository = new ProdutoRepository();
+        produtoRepository.atualizarProduto(produto,id);
+        System.out.println("Produto Atualizado com sucesso!");
+    }
+
+    public static void excluirProduto(){
+
+        Scanner leia = new Scanner(System.in);
+        int id;
+        System.out.println("Id do produto a ser excluído: ");
+        id = leia.nextInt();
+
+        ProdutoRepository produtoRepository = new ProdutoRepository();
+        produtoRepository.excluirProduto(id);
+
+        System.out.println("Produto excluído!");
+
+    }
+
 }
